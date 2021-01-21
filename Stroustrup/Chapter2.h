3775fa1413d3b25a3b2ffc3dad3a150df977d2c4
Steve Worcester <Worcester.Steve@Gmail.com>
2021-01-20 17:53:52 -0800

    ch2 complete

---
+++ Stroustrup/Chapter2.h
@@ -0,0 +1,13 @@
+#include "std_lib_facilities.h"
+
+static class Chapter2
+{
+public:
+    static int RunChapter2() {
+        cout << "Hello, programming!\n";
+        cout << "Here we go!\n";
+        keep_window_open();
+        return 0;
+    }
+};
+
